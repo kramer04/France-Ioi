8cbc20566c9be575b894922bd1097dfc78c961d6
kramer04 <duncan4031@gmail.com>
2024-06-18 18:48:46 +0200

    Delete Niveau 1/Plan de la montagne directory

--- Niveau 1/Plan de la montagne/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(){
-   std::cout<<"Tout droit tu grimperas,\nLa clé tu trouveras,\nHabile tu seras,\nQuand tu les porteras,\nEt avec le chef tu reviendras !"<<std::endl;
-   return 0;
-}
