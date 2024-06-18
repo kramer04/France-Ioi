c9ebd9f7d8e08e7a1c1cc2e28e3fc92ca56e1fa8
kramer04 <duncan4031@gmail.com>
2024-06-18 19:12:10 +0200

    Create main.cpp

---
+++ Niveau 1/Répétitions d'instructions/Mont Kailash/main.cpp
@@ -0,0 +1,25 @@
+#include "robot.h"
+#define repeat(nb) for (int _loop = 1, _max = (nb); _loop <= _max; _loop++)
+int main()
+{
+   repeat(108)
+   {
+   repeat(13)
+   {
+    haut();
+   }
+   repeat(13)
+   {
+    droite();
+   }
+   repeat(13)
+   {
+    bas();
+   }
+   repeat(13)
+   {
+    gauche();
+   }
+   }
+   return 0;
+}
