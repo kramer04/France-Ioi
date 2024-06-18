fe7330dbddd28753b21882b9764aa6580d2ca6f0
kramer04 <duncan4031@gmail.com>
2024-06-18 19:22:03 +0200

    Create main.cpp

---
+++ Niveau 1/Calculs et découverte des variables/Progresser par l'erreur/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   std::cout<< "V" << std::endl;
+   std::cout<< "V" << std::endl;
+   std::cout<< "I" << std::endl;
+   std::cout<< "I" << std::endl;
+   std::cout<< "V" << std::endl;
+   std::cout<< "I" << std::endl;
+   std::cout<< "I" << std::endl;
+}
