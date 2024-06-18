4ca95d8768825c63d7da42e9f54c8321ed95f424
kramer04 <duncan4031@gmail.com>
2024-06-18 19:09:08 +0200

    Create main.cpp

---
+++ Niveau 1/Répétitions d'instructions/Sisyphe/main.cpp
@@ -0,0 +1,12 @@
+#include "robot.h"
+#define repeat(nb) for (int _loop = 1, _max = (nb); _loop <= _max; _loop++)
+int main(){
+repeat(21){
+haut();
+droite();
+}
+repeat(21){
+gauche();
+bas();
+}
+}
