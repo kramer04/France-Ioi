9da830f55a19dbfca50b850788a3e2308c48149d
kramer04 <duncan4031@gmail.com>
2024-06-18 18:44:24 +0200

    Create main.cpp
    Plan de la montagne

---
+++ Niveau 1/Plan de la montagne/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+   std::cout<<"Tout droit tu grimperas,\nLa clé tu trouveras,\nHabile tu seras,\nQuand tu les porteras,\nEt avec le chef tu reviendras !"<<std::endl;
+   return 0;
+}
