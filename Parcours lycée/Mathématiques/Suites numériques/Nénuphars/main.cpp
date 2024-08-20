c7f3f1e6df90ecd626cca3230309c51f772b60a6
kramer04 <duncan4031@gmail.com>
2024-08-20 14:55:45 +0200

    Update main.cpp

--- Parcours lycée/Mathématiques/Suites numériques/Nénuphars/main.cpp
+++ Parcours lycée/Mathématiques/Suites numériques/Nénuphars/main.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+int main()
+{
+  int n, resultat {2};
+  std::cin >> n;
+  for (int i {0}; i < n; i++)
+  {
+    std::cout << std::fixed << std::setprecision(0) << std::pow(resultat, i) << '\n';
+  }
+  return 0;
+}
