730ca8b32c1728aca74b316989df42f22a5afa9d
kramer04 <duncan4031@gmail.com>
2024-08-20 20:38:33 +0200

    Update main.cpp

--- Parcours lycée/Mathématiques/Algorithmique du baccalauréat/Calcul de Un/main.cpp
+++ Parcours lycée/Mathématiques/Algorithmique du baccalauréat/Calcul de Un/main.cpp
@@ -1,1 +1,15 @@
-//à venir
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+int main()
+{
+  int n;
+  std::cin >> n;
+  double s {0.0};
+  for (int i {1}; i <= n; ++i)
+  {
+    s += 1.0 / i;
+  }
+  s -= log(n);
+  std::cout << std::fixed << std::setprecision(6) << s << '\n';
+}
