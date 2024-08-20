deb1d3f93af129eadb4ba015c4b0044e31ebd440
kramer04 <duncan4031@gmail.com>
2024-08-20 23:37:10 +0200

    Update main.cpp

--- Parcours lycée/Mathématiques/Algorithmique du baccalauréat/Affichage sans doublons/main.cpp
+++ Parcours lycée/Mathématiques/Algorithmique du baccalauréat/Affichage sans doublons/main.cpp
@@ -1,1 +1,19 @@
-//A venir
+#include <iostream>
+#include <cmath>
+int main()
+{
+  int A, N {1};
+  std::cin >> A;
+  while (N <= std::sqrt(A))
+  {
+    if (!(A % N))
+    {
+      if (N != A / N)
+        std::cout << N << '\n' << A / N << '\n';
+      else
+        std::cout << N << '\n';
+    }
+    N++;
+  }
+  return 0;
+}
